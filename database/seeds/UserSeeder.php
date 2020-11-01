3db21a2bba11b0f5a3ac4d0f10347817f5ac07eb
Valeria Rojas <valeria.rojas.chacon@gmail.com>
2020-10-31 20:04:57 -0600

    1

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'valro@gmail.com',
+            'password' => '12345678',
+        ]);
+    }
+}
